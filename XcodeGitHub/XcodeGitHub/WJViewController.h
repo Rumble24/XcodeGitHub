bce33d70bbf4c67621f00e64a11a510fa031ce42
jingwei <496116842@qq.com>
2018-04-27 10:38:04 +0800

    随便加了几个文件

---
+++ XcodeGitHub/XcodeGitHub/WJViewController.h
@@ -0,0 +1,13 @@
+//
+//  WJViewController.h
+//  XcodeGitHub
+//
+//  Created by toushiro0000 on 2018/4/27.
+//  Copyright © 2018年 宜必鑫科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WJViewController : UIViewController
+
+@end
---
+++ XcodeGitHub/XcodeGitHub/WJViewController.m
@@ -0,0 +1,37 @@
+//
+//  WJViewController.m
+//  XcodeGitHub
+//
+//  Created by toushiro0000 on 2018/4/27.
+//  Copyright © 2018年 宜必鑫科技. All rights reserved.
+//
+
+#import "WJViewController.h"
+
+@interface WJViewController ()
+
+@end
+
+@implementation WJViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
